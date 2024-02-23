ef17d1452df13ce5777a37b37afb4e51abb351ca
Sara-Clarin <sclarin2@nd.edu>
2024-02-23 10:19:38 -0800

    change randfile to generate stdout stream by default

--- randfile.py
+++ randfile.py
@@ -1,4 +1,3 @@
-import sys
 import random
 import argparse
 
@@ -63,11 +62,9 @@ def write_diff(new_file, diff):
 # output: randomized list of words, with the word-lengths interleaved
 def choose_words( eights, fours, twos, ones,nbytes, new_file):
     words = []
-
     sum = eights + fours + twos + ones
 
     tchars = sum + (eights*8) + (fours*4) + (twos*2) + ones - 1
-    print(f'Total chars: {tchars}')
 
     i = 0
     while sum > 0:
@@ -114,10 +111,88 @@ def choose_words( eights, fours, twos, ones,nbytes, new_file):
     new_file.close()
     return
 
-def generate_new_file( nbytes):
-    print(f"Generating random text file of length {nbytes} bytes\n")
+'''
+Print_diff and print_words together do the same functionality as write_diff and choose_words
+These print to the screen without any file I/O
+--Note that duplicating the functions is faster than logic blocks within one function for I/O
+'''
+def print_diff( diff):
+    w1 = words_fourb[make_choice("4")]
+    w2 = words_twob[make_choice("2")]
+    w3 = words_twob[make_choice("2")]
+    w4 = words_oneb[make_choice("1")]
+    if diff == 7:
+        print(f'{w1} {w2}',end='')
+    elif diff == 6:
+        print(f'{w1}{w2}', end='')
+    elif diff == 5:
+        print(f'{w2} {w3}', end='')
+    elif diff == 4:
+        print(f'{w1}', end='')
+    elif diff == 3:
+        print(f'{w3} ', end='')
+    elif diff == 2:
+        print(f'{w3}', end='')
+    elif diff == 1:
+        print(f'{w4}', end='')
+    return
 
-    new_file = open(f'{nbytes}.txt', "w+")
+def print_words(eights, fours, twos, ones, nbytes):
+    words = []
+    sum = eights + fours + twos + ones
+
+    tchars = sum + (eights*8) + (fours*4) + (twos*2) + ones - 1
+    #print(f'Total chars: {tchars}')
+
+    i = 0
+    while sum > 0:
+
+        if eights > 0:
+            words.append(words_eightb[ make_choice("8")])
+            eights -= 1
+        if fours > 0 and i%2:
+            words.append(words_fourb[ make_choice("4")])
+            fours -= 1
+        if twos > 0:
+            words.append(words_twob[ make_choice("2")])
+            twos -= 1
+        if ones > 0:
+            words.append(words_oneb[ make_choice("1")])
+            ones -= 1
+
+        sum = eights + fours + twos + ones
+        i += 1
+        
+    total_chars = 0
+    for i, word in enumerate(words):
+        total_chars += len(word)     # what we would be at if we wrote this
+
+        if total_chars + 1 <= nbytes:
+            print(f'{word} ', end='')
+            total_chars += 1        # account for the space
+        elif total_chars == nbytes:
+            print(f'{word}',end='')
+        else:
+            diff = nbytes - (total_chars - len(word)) #how many left to write
+            #print(f'Diff is: {diff}')
+            if diff > 0:                    # add small number of bytes
+                print_diff( diff)
+                break
+            if diff == 0:
+                break
+            elif diff < 0:                     # truncate file
+                diff *= -1 
+                end = len(word) - diff
+                print(f'{word[0:end]}', end='')
+                break
+
+    return
+
+def generate_new_file( nbytes, to_stdout):
+    if not to_stdout:
+        print(f"Generating random text file of length {nbytes} bytes\n")
+
+        new_file = open(f'{nbytes}.txt', "w+")
 
     num_eightbs = nbytes  // 8
     remainder = nbytes - (num_eightbs * 8)
@@ -145,11 +220,13 @@ def generate_new_file( nbytes):
         remainder -= num_ones + num_ones
         i +=  1
     
-    print(f'Iterations: {i}')
-    print(f'statistics: eights - {num_eightbs} fours- {tot4s}, twos: {tot2s} ones: {tot1s}')
+    #print(f'Iterations: {i}')
+    #print(f'statistics: eights - {num_eightbs} fours- {tot4s}, twos: {tot2s} ones: {tot1s}')
 
-    to_write = choose_words( num_eightbs, num_fours, num_twos, num_ones, nbytes, new_file)
-
+    if not to_stdout:
+        to_write = choose_words( num_eightbs, num_fours, num_twos, num_ones, nbytes, new_file)
+    else:
+        to_write = print_words(num_eightbs, num_fours, num_twos, num_ones, nbytes)
 def usage():
     print("Error -- Usage: randfile.py [number of bytes:]")
 
@@ -157,9 +234,10 @@ def main():
   
     parser = argparse.ArgumentParser()
     parser.add_argument("numbytes", help="Number of bytes requested", type=int)
+    parser.add_argument("--to_stdout", required=False, default=1, type=int)
     args = parser.parse_args()
 
-    generate_new_file( args.numbytes)
+    generate_new_file( args.numbytes, args.to_stdout)
 
 if __name__ == "__main__":
     main()
