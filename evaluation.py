de0f9d3c945b71f9bc93408e29e60b2ac620a1ba
Sara-Clarin <sclarin2@nd.edu>
2024-03-14 11:09:06 -0700

    Add variable scaling to measurements

--- evaluation.py
+++ evaluation.py
@@ -42,22 +42,24 @@ def iso_iec_7816_4_pad(pt):
 def GenFileSet():
 
     print("Generating test files....")
+    scalar = 10**3
     # Could move these to globals if need be
-    fsize = 32 * (10**1) #16 megabyte file
-    step   = 32* (10**1)
+    fsize = 1 * scalar 
+    step  = 16 * scalar
 
     for i in range(8):
         print(f"Creating file of size {fsize} .......")
 
         subprocess.run(["python3", "randfile.py", f'{fsize}', '--to_stdout', '0'])
-        fsize += step
+        fsize += step - scalar # To keep the numbers even multiples
 
 def Run_Enc_Analysis( ):
 
     # TODO: generate keys beforehand and extract them
+    scalar = 10 ** 3
 
-    fsize = 32 * (10**1) #16 megabyte file
-    step  = 32* (10**1)
+    fsize = 1 * scalar
+    step  = 16 * scalar
 
     for i in range(8):
         print(f"Testing {fsize} .......")
@@ -84,12 +86,10 @@ def Run_Enc_Analysis( ):
         for x in range(num_blocks):
             block = padded[x*16: (x*16)+16]
             aesencrypt.aes_encrypt(block, key)   # do not store ciphertext for speed
-            print(f'[INFO]: Blocks remaining: {num_blocks - x}')
-            
-        file_sizes.append(fsize)
-
+            #print(f'[INFO]: Blocks remaining: {num_blocks - x}')
 
         sequential_time.append(time.time_ns() - start)
+        file_sizes.append(fsize // scalar )
 
         ###############
         #TODO: PARALLEL AES
@@ -99,23 +99,25 @@ def Run_Enc_Analysis( ):
         #parallel_time.append( time.time_ns() - start)
         parallel_time.append(0)
 
-        fsize += step
+        fsize += step - scalar 
 
-    print(f'sequential time: {sequential_time}')
-    print(f'File sizes: {file_sizes}')
+    #print(f'sequential time: {sequential_time}')
+    #print(f'File sizes: {file_sizes}')
 
 # create a bar chart
 # If this doesn't work, then a CSV 
 def Plot_bar(xvals, yvals1, yvals2):
 
-    barWidth = 100
+    barWidth = 0.25
 
     #file_sizes on x axis, 
     plt.bar(xvals, yvals1, color='blue', width=barWidth)
     #plt.bar(xvals, yvals2, color='red')
 
-    plt.ylabel("Execution time ")
-    plt.xlabel("File size")
+    plt.ylabel("Execution time (seconds)")
+    #plt.xlabel("File size (MB)")
+    #plt.xlabel("File size (bytes)")
+    plt.xlabel("File size (KB)")
     plt.show()
     
 # Option to examine line graph
